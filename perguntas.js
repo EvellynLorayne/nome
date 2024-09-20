1efc7c0b951b0ce99866d995641d601601ee3f73
EvellynLorayne <evellynbonifacio6@gmail.com>
2024-09-20 11:52:17 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'O Grito',
+    'A Obra O Grito, é de qual pintor famoso?',
+    'Edvard Munch'
+)
+
+criaCartao(
+    'Noite Estrelada',
+    'A Obra Noite Estrelada pertence a que pintor famoso?',
+    'Vincent Van Gogh'
+)
+
+criaCartao(
+    'Ronda Noturna',
+    'A Obra Ronda Noturna foi pintada por?',
+    'Rembrandt'
+)
+
+criaCartao(
+    'O Nascimento de Venus',
+    'A Obra O Nascimento de Venus pertence a quem?',
+    'Botticelli)'
+)
